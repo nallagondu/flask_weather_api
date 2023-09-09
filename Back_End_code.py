a6913a37815021f633333bb3f499c6d092e39cc6
rohi92 <rohi92>
2023-09-09 05:22:15 +0000

    Updated the changes to Backend_Code

--- Back_End_code.py
+++ Back_End_code.py
@@ -1,19 +1,36 @@
 # Databricks notebook source
 from delta.tables import *
-from pyspark.sql.functions import *
-import pyspark.sql.functions as F
+from pyspark.sql import functions as F
 import json
+import datetime
 from pyspark.sql.functions import when
+from pyspark.sql.functions  import *
 
 # COMMAND ----------
 
-dbutils.widgets.text('user_input', "[{'city_id': 1, 'year': 2017, 'month': 1, 'day': 1, 'hour': 100}, {'city_id': 1, 'end_year': 2017, 'end_month': 1, 'end_day': 2, 'end_hour': 600}]")
-input=dbutils.widgets.get("user_input")
-#dbutils.widgets.removeAll()
+current_time_stamp=str(datetime.datetime.today())
+
+# COMMAND ----------
+
+current_time_stamp
+
+# COMMAND ----------
+
+dbutils.widgets.text('list1', "[{'city_id': 1, 'year': 2017, 'month': 1, 'day': 1, 'hour': 100}, {'city_id': 1, 'end_year': 2017, 'end_month': 2, 'end_day': 15, 'end_hour': 600}]")
+input=dbutils.widgets.get("list1")
+dbutils.widgets.removeAll()
+
+# COMMAND ----------
+
+type(input)
 
 # COMMAND ----------
 
 l1=eval(input)
+
+# COMMAND ----------
+
+type(l1)
 
 # COMMAND ----------
 
@@ -63,8 +80,12 @@ df1.display()
 
 if start_month<9:
     start_month1='0'+str(start_month)
+else:
+    start_month1=start_month
 if start_day<9:
     start_day1='0'+str(start_day)
+else:
+    start_day1=start_day
 
 start_date=str(start_year)+'-'+str(start_month1)+'-'+str(start_day1)
 
@@ -72,22 +93,36 @@ start_date=str(start_year)+'-'+str(start_month1)+'-'+str(start_day1)
 
 if end_month<9:
     end_month1='0'+str(end_month)
+else:
+    end_month1=end_month
 if end_day<9:
     end_day1='0'+str(end_day)
+else:
+    end_day1=end_day
 
-end_date=str(end_year)+'-'+str(end_month1)+'-'+str(end_day1)
 
-# COMMAND ----------
 
-df2=df1.filter((df1.city_id==city_id)&(df1.date>=(lit(start_date)) ) & (df1.date<=lit(end_date)) )
-
-# COMMAND ----------
-
-df2.display()
+end_date=str(start_year)+'-'+str(end_month1)+'-'+str(end_day1)
 
 # COMMAND ----------
 
-df3=df2.withColumn("cond", when ((df2.date==start_date) & (df2.hour< start_hour), 1).when ((df2.date==end_date) & (df2.hour>end_hour),1).otherwise(0))
+start_date
+
+# COMMAND ----------
+
+end_date
+
+# COMMAND ----------
+
+df1=df1.filter((df1.city_id==city_id)& (df1.date>= start_date) & (df1.date<=end_date) )
+
+# COMMAND ----------
+
+df2=df1.withColumn("cond", when ((df1.date==start_date) & (df1.hour< start_hour), 1).when ((df1.date==end_date) & (df1.hour>end_hour),1).otherwise(0))
+
+# COMMAND ----------
+
+df3=df2.filter(df2.cond==0)
 
 # COMMAND ----------
 
@@ -95,24 +130,12 @@ df3.display()
 
 # COMMAND ----------
 
-df3=df3.filter(df3.cond==0)
+current_time_stamp=current_time_stamp.replace(":","")
 
 # COMMAND ----------
 
-df3.display()
+df2.write.save("/mnt/replication/weather_request/"+current_time_stamp)
 
 # COMMAND ----------
 
-from datetime import *
-
-# COMMAND ----------
-
-current_timestamp=datetime.today()
-
-# COMMAND ----------
-
-current_timestamp=str(current_timestamp).replace(":","")
-
-# COMMAND ----------
-
-df3.write.save("/mnt/replication/weather_request"+current_timestamp)
+dbutils.fs.ls("/mnt/replication/weather_request")
